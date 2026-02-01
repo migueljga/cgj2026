b809c619549aa696d038a709f4eefe28e46fe2a7
Erick Subero <suberoerick@gmail.com>
2026-02-01 12:13:55 -0400

    Horizontal Animation

--- Assets/Scripts/Utils/VerticalJumpyAnimation.cs
+++ Assets/Scripts/Utils/VerticalJumpyAnimation.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 
 public class VerticalJumpyAnimation : MonoBehaviour
 {
+    [SerializeField] private Orientation orientation = Orientation.Y;
     [SerializeField] private float maxValue = 1.3f;
     [SerializeField] private float speed = 0.5f;
     [SerializeField] private LoopType loop = LoopType.Yoyo;
     [SerializeField] private Ease easing = Ease.InOutQuad;
 
+    public enum Orientation { X, Y }
+
     private void Start()
     {
-        transform.DOLocalMoveY(maxValue, speed)
-         .SetLoops(-1, loop)
-         .SetEase(easing);
+        switch (orientation)
+        {
+            case Orientation.X:
+                transform.DOLocalMoveX(maxValue, speed)
+                 .SetLoops(-1, loop)
+                 .SetEase(easing);
+                break;
+            case Orientation.Y:
+            default:
+                transform.DOLocalMoveY(maxValue, speed)
+                 .SetLoops(-1, loop)
+                 .SetEase(easing);
+                break;
+        }
+            
     }
 }
